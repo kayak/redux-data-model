b9cb52bb841bbcad7b24204d4f3308f02aa37faf
Lucas Lira Gomes <llira@kayak.com>
2019-08-01 13:16:50 +0200

    docs: add docz documentation

---
+++ doczrc.js
@@ -0,0 +1,9 @@
+export default {
+  typescript: true,
+  title: 'Kaytum',
+  dest: './public/',
+  themeConfig: {
+    mode: 'dark',
+  },
+  menu: ['Getting Started', 'Concepts', 'Integrating with Redux', 'Development'],
+}
