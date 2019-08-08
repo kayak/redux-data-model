bd06e46f2a2acde5680a64c4c9ecb3544b0516ae
Lucas Lira Gomes <llira@kayak.com>
2019-08-08 13:37:05 +0200

    docs: add documentation for hooks and fix deploy script

--- doczrc.js
+++ doczrc.js
@@ -1,8 +1,10 @@
 export default {
   typescript: true,
   title: 'Kaytum',
+  description: 'Opinionated, Redux abstraction with built-in immutability, normalization, and selectors support',
+  base: '/kaytum/',
   themeConfig: {
     mode: 'dark',
   },
-  menu: ['Getting Started', 'Concepts', 'Integrating with Redux', 'Development'],
+  menu: ['Getting Started', 'Concepts', 'Hooks', 'Integrating with Redux', 'Development'],
 }
