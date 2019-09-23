38363369d101adcf15f9949633f1f94d4e7b5552
Lucas Lira Gomes <llira@kayak.com>
2019-09-23 18:00:42 +0200

    chore: add a new example with sweetalert

---
+++ examples/counterWithConnectResuxAndConfirmationDialog/alerts.js
@@ -0,0 +1,25 @@
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const ReactSwal = withReactContent(Swal).mixin({
+  customClass: {
+    confirmButton: "btn btn-lg btn-primary mx-2 px-5",
+    cancelButton: "btn btn-lg btn-secondary mx-2 px-5"
+  },
+  buttonsStyling: false
+});
+
+const alert = options => ReactSwal.fire(options);
+
+const confirm = (options = {}) =>
+  alert({
+    showCancelButton: true,
+    showConfirmButton: true,
+    ...options
+  });
+
+export async function showConfirm(options) {
+  const result = await confirm(options);
+  if (result.value !== true) return false;
+  return true;
+}
---
+++ examples/counterWithConnectResuxAndConfirmationDialog/pages/index.jsx
@@ -0,0 +1,109 @@
+import {
+  combineModelReducers,
+  connectResux,
+  Model,
+  resuxRootSaga,
+} from 'react-resux';
+import createSagaMiddleware from 'redux-saga';
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+} from 'redux';
+import logger from 'redux-logger';
+import {
+  Provider,
+  useSelector,
+} from 'react-redux';
+import * as React from 'react';
+import JSONTree from 'react-json-tree';
+import {showConfirm} from '../alerts';
+
+export const counterModel = new Model({
+  namespace: "counter",
+  state: {
+    count: 0
+  },
+  selectors: {
+    count: state => state.counter.count
+  },
+  reducers: {
+    increment(state, {}) {
+      state.count += 1;
+    },
+    decrement(state, {}) {
+      state.count -= 1;
+    }
+  },
+  effects: {
+    *tryToIncrement(action, sagaEffects) {
+      const hasConfirmed = yield sagaEffects.call(showConfirm, {
+        text: "Are you sure you want to increment?"
+      });
+
+      if (hasConfirmed) yield sagaEffects.put({ type: "counter.increment" });
+    },
+    *tryToDecrement(action, sagaEffects) {
+      const hasConfirmed = yield sagaEffects.call(showConfirm, {
+        text: "Are you sure you want to decrement?"
+      });
+
+      if (hasConfirmed) yield sagaEffects.put({ type: "counter.decrement" });
+    }
+  }
+});
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  combineReducers({
+    ...combineModelReducers([counterModel])
+  }),
+  applyMiddleware(logger, sagaMiddleware)
+);
+
+sagaMiddleware.run(() => resuxRootSaga([counterModel]));
+
+function TestComponent({ count, counter }) {
+  // Only used for displaying entire state
+  const allState = useSelector(state => state);
+
+  return (
+    <>
+      <div>
+        <strong>Count:</strong> {count}
+      </div>
+      <div>
+        <button onClick={() => counter.tryToIncrement()}>Increment</button> |{" "}
+        <button onClick={() => counter.tryToDecrement()}>Decrement</button>
+      </div>
+      <br />
+      <hr />
+      <br />
+      <div>
+        <strong>
+          State (Open console to see actions as they are triggered):
+        </strong>
+        <JSONTree data={allState} />
+      </div>
+    </>
+  );
+}
+
+function mapStateToProps(state, props, selectors) {
+  return {
+    count: selectors.counter.count(state)
+  };
+}
+
+const WrappedTestComponent = connectResux([counterModel], mapStateToProps)(
+  TestComponent
+);
+
+export default () => {
+  return (
+    <Provider store={store}>
+      <WrappedTestComponent />
+    </Provider>
+  );
+};
