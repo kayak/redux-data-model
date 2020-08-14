5454adfe69974a662680263036cf7c190c0dbb57
Lucas Lira Gomes <llira@kayak.com>
2020-08-14 10:49:26 +0200

    build: change setup to expose types and deprecate tslint usage

--- packages/redux-data-model/rollup.config.js
+++ packages/redux-data-model/rollup.config.js
@@ -1,19 +1,4 @@
 import pkg from './package.json';
 import baseConfig from '../../rollup.config';
 
-export default baseConfig({
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-});
+export default baseConfig({}, pkg);
--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,20 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
+import dts from "rollup-plugin-dts";
 
-export default props => ({
+export default (props, pkg) => ([{
   input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
       format: 'cjs',
+      sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
+      sourcemap: true,
     },
   ],
   external: [
@@ -24,9 +26,15 @@ export default props => ({
     resolve(),
     commonjs(),
     typescript({
-      typescript: require('typescript'),
+      typescript: require('typescript'), tsconfigOverride: { compilerOptions: { declaration: false } },
     }),
     filesize(),
   ],
   ...props,
-});
+},
+{
+  input: "src/index.ts",
+  output: [{ file: pkg.typings, format: "es" }],
+  plugins: [dts()],
+}
+]);
